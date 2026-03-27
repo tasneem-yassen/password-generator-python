8c946b5bc40efcf5167fd72e67215445d6d4343d
tasneem-yassen <tasneem5872@gmail.com>
2026-03-28 00:45:33 +0200

    add lowercase letters option with CLI flag and input validation

--- generator.py
+++ generator.py
@@ -2,17 +2,26 @@ import secrets #Use secrets for cryptographic randomness
 import argparse
 
 
-def password_generator(length): 
+def password_generator(length,lowcase): 
     counter = 0 
     digits = "0123456789"
+    digits_letters = "0123456789abcdefghijklmnopqrstuvwxyz"
     password = ""
     while counter < length: 
-        password += secrets.choice(digits)
+        if lowcase : 
+            password += secrets.choice(digits_letters)
+        else:
+            password += secrets.choice(digits)
         counter+=1 
     print("your password:",password)
 if __name__ == "__main__": 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument("--length",type=int,required=True)
+    parser.add_argument("--lowercase_letters",action="store_true")
     args = parser.parse_args()
     pass_length = args.length 
-    password_generator(pass_length)
+    include_lowcase = args.lowercase_letters
+    if pass_length <=0 : 
+        print("Length must be positive")
+    else : 
+        password_generator(pass_length,include_lowcase)
