b78f567ff807d5a5b4f81e6c1cc0b8bf5602bca4
tasneem-yassen <tasneem5872@gmail.com>
2026-03-28 00:20:08 +0200

    add CLI password generator using secrets (digits only)

--- generator.py
+++ generator.py
@@ -1,1 +1,18 @@
-print("start")
+import secrets #Use secrets for cryptographic randomness
+import argparse
+
+
+def password_generator(length): 
+    counter = 0 
+    digits = "0123456789"
+    password = ""
+    while counter < length: 
+        password += secrets.choice(digits)
+        counter+=1 
+    print("your password:",password)
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--length",type=int,required=True)
+    args = parser.parse_args()
+    pass_length = args.length 
+    password_generator(pass_length)
