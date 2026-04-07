de22ccad2eec9bd9559eb1cf8ea238f5f11959c7
tasneem-yassen <tasneem5872@gmail.com>
2026-04-07 22:56:27 +0300

    add uppercase letters option with CLI flag and input validation

--- generator.py
+++ generator.py
@@ -2,26 +2,31 @@ import secrets #Use secrets for cryptographic randomness
 import argparse
 
 
-def password_generator(length,lowcase): 
+def password_generator(length,lowcase,uppercase): 
     counter = 0 
     digits = "0123456789"
-    digits_letters = "0123456789abcdefghijklmnopqrstuvwxyz"
+    lower_letters = "abcdefghijklmnopqrstuvwxyz"
+    upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     password = ""
+    pool = digits 
+    if uppercase :
+        pool += upper_letters 
+    if lowcase : 
+        pool += lower_letters
     while counter < length: 
-        if lowcase : 
-            password += secrets.choice(digits_letters)
-        else:
-            password += secrets.choice(digits)
+        password += secrets.choice(pool)
         counter+=1 
     print("your password:",password)
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument("--length",type=int,required=True)
     parser.add_argument("--lowercase_letters",action="store_true")
+    parser.add_argument("--uppercase_letters",action="store_true")
     args = parser.parse_args()
     pass_length = args.length 
     include_lowcase = args.lowercase_letters
+    include_uppercase = args.uppercase_letters 
     if pass_length <=0 : 
         print("Length must be positive")
     else : 
-        password_generator(pass_length,include_lowcase)
+        password_generator(pass_length,include_lowcase,include_uppercase)
